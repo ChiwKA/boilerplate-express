0c10bce8d5feb9e8b87e4fe3cc4ef62e0f234d25
ChiwKA <hoangquan96@gmail.com>
2025-07-05 14:37:45 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,13 +1,18 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
-console.log("Hello World");
 // app.get("/", function(req, res) {
 //   res.sendFile(__dirname + '/views/index.html')
 // })
 
 app.get('/json', (req, res) => {
-  res.json({"message": "Hello json"})
+  console.log(process.env.MESSAGE_STYLE)
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": "HELLO JSON"})
+  } else {
+    res.json({"message": "Hello json"})
+  }
 })
 
 app.use('/public', express.static(__dirname + '/public'))
