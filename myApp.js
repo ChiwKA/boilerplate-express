653805fbef2e00c1656facb437697c130a88c005
ChiwKA <152947610+ChiwKA@users.noreply.github.com>
2025-07-05 13:30:44 +0700

    5/7

--- myApp.js
+++ myApp.js
@@ -2,9 +2,17 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
-app.get("/", function(req, res) {
-  res.send("Hello Express")
+// app.get("/", function(req, res) {
+//   res.sendFile(__dirname + '/views/index.html')
+// })
+
+app.get('/json', (req, res) => {
+  res.json({"message": "Hello json"})
 })
+
+app.use('/public', express.static(__dirname + '/public'))
+
+
 
 
 
